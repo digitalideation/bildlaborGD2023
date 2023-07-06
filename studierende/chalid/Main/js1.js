f00b3735a206bf5559e0b03381116cf53939f47c
chalidbaumann <104133122+chalidbaumann@users.noreply.github.com>
2023-07-06 19:08:52 +0200

    Add files via upload

--- studierende/chalid/Main/js1.js
+++ studierende/chalid/Main/js1.js
@@ -58,27 +58,22 @@
 
 
 
-//line animation - Schlüsselwörter
-/////*https://www.youtube.com/watch?v=fa8kZNhdHOk
-let line1 = document.querySelector('.line-1');
-let line2 = document.querySelector('.line-2');
-let line3 = document.querySelector('.line-3');
-let line4 = document.querySelector('.line-4');
-let keywordsContainer = document.querySelector('#keywords');
+// //line animation - Schlüsselwörter
+// ///*https://www.youtube.com/watch?v=fa8kZNhdHOk
+// let line1 = document.querySelector('.line-1');
+// let line2 = document.querySelector('.line-2');
+// let line3 = document.querySelector('.line-3');
+// let line4 = document.querySelector('.line-4');
+// let keywordsContainer = document.querySelector('#keywords');
 
 
-window.onscroll = () =>{
-  const topOffset = keywordsContainer.getBoundingClientRect().top - -600;
-  line1.style.left = `${topOffset}px`
-  line2.style.right = `${topOffset}px`
-  line3.style.left = `${topOffset}px`
-  line4.style.right = `${topOffset}px`
-
-  }
-
-
-
-
+// window.onscroll = () =>{
+//   const topOffset = keywordsContainer.getBoundingClientRect().top - -600;
+//   line1.style.left = `${topOffset}px`
+//   line2.style.right = `${topOffset}px`
+//   line3.style.left = `${topOffset}px`
+//   line4.style.right = `${topOffset}px`
+//   }
 
 
 
@@ -101,54 +96,54 @@ window.onscroll = () =>{
 
 
 
-  //Funktioniert nicht!!!!!!!!
-  //Magnifier Glass
-  //https://www.youtube.com/watch?v=ylYDtARRmOo
- function magnify(imgID, zoom){
-  var img, glass, w, h, bw;
-  img = document.getElementById(imgID);
-  glass = document.createElement("div");
-  glass.setAttribute("class", "img-magnifier-glass");
-  img.parentElement.insertBefore(glass, img);
-  glass.style.backgroundImage = "url('" + img.scr+"')";
-  glass.style.backgroundRepeat = "no-repeat";
-  glass.style.backgroundSize = (img.width*zoom)+ "px" +(img.height*zoom) + "px";
+//   //Funktioniert nicht!!!!!!!!
+//   //Magnifier Glass
+//   //https://www.youtube.com/watch?v=ylYDtARRmOo
+//  function magnify(imgID, zoom){
+//   var img, glass, w, h, bw;
+//   img = document.getElementById(imgID);
+//   glass = document.createElement("div");
+//   glass.setAttribute("class", "img-magnifier-glass");
+//   img.parentElement.insertBefore(glass, img);
+//   glass.style.backgroundImage = "url('" + img.scr+"')";
+//   glass.style.backgroundRepeat = "no-repeat";
+//   glass.style.backgroundSize = (img.width*zoom)+ "px" +(img.height*zoom) + "px";
 
-  bw=3;
-  w= glass.offsetWidth/2;
-  h= glass.offsetHeight/2;
-  glass.addEventListener("mousemove", moveMagnifier);
-  img.addEventListener("mousemove", moveMagnifier);
+//   bw=3;
+//   w= glass.offsetWidth/2;
+//   h= glass.offsetHeight/2;
+//   glass.addEventListener("mousemove", moveMagnifier);
+//   img.addEventListener("mousemove", moveMagnifier);
 
-  glass.addEventListener("touchmove", moveMagnifier);
-  img.addEventListener("touchmove", moveMagnifier);
+//   glass.addEventListener("touchmove", moveMagnifier);
+//   img.addEventListener("touchmove", moveMagnifier);
 
-  function moveMagnifier(e){
-    var pos, x, y;
+//   function moveMagnifier(e){
+//     var pos, x, y;
 
-    e.proventDefault();
+//     e.proventDefault();
 
-    pos = getcursorpos(e);
-    x=pos.x;
-    y=pos.y;
+//     pos = getcursorpos(e);
+//     x=pos.x;
+//     y=pos.y;
 
 
-    if(x> img.width -(w/zoom)){
-      x = img.width -(w/zoom);
-    }
-    if (x< w/zoom){
-      x = w/zoom;
-    }
-    if (y> img.height - (h/zoom)){
-    y = img.height-(h/zoom);
-}
-    if (y< h/zoom){
-    y = h/zoom;
+//     if(x> img.width -(w/zoom)){
+//       x = img.width -(w/zoom);
+//     }
+//     if (x< w/zoom){
+//       x = w/zoom;
+//     }
+//     if (y> img.height - (h/zoom)){
+//     y = img.height-(h/zoom);
+// }
+//     if (y< h/zoom){
+//     y = h/zoom;
 
-}
+// }
 
-  }
-}
+//   }
+// }
 
 
 
